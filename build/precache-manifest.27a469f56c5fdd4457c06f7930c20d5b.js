96be2aeebaef7d95b25dac2895aaafd5c50a7369
sarapourabbas <sara.poorabas@gmail.com>
2021-09-03 13:08:34 +0430

    update project

---
+++ build/precache-manifest.27a469f56c5fdd4457c06f7930c20d5b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf71efcc40ce993fb28dae62add75ef",
+    "url": "/Map/index.html"
+  },
+  {
+    "revision": "2c50843d2b481df12ed8",
+    "url": "/Map/static/css/2.508ae934.chunk.css"
+  },
+  {
+    "revision": "fe9c6237db12ddcd1932",
+    "url": "/Map/static/css/main.268b023d.chunk.css"
+  },
+  {
+    "revision": "2c50843d2b481df12ed8",
+    "url": "/Map/static/js/2.2b8d8933.chunk.js"
+  },
+  {
+    "revision": "fe9c6237db12ddcd1932",
+    "url": "/Map/static/js/main.5a6ea91f.chunk.js"
+  },
+  {
+    "revision": "259e26965257e61dd34b",
+    "url": "/Map/static/js/runtime~main.504d9156.js"
+  },
+  {
+    "revision": "ac22d187130d6c3433a49a1e98bfa968",
+    "url": "/Map/static/media/IRANSansWeb.ac22d187.ttf"
+  },
+  {
+    "revision": "c6f46673e2449eb071e2c098c5190c2a",
+    "url": "/Map/static/media/IRANSansWeb.c6f46673.woff2"
+  },
+  {
+    "revision": "df14582918ca379a280e453bb3cc6ba5",
+    "url": "/Map/static/media/IRANSansWeb.df145829.woff"
+  },
+  {
+    "revision": "e43cfbc1a67d90e910398ded8345cd32",
+    "url": "/Map/static/media/IRANSansWeb.e43cfbc1.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Map/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Map/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Map/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Map/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Map/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
